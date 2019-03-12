31c6b723ea930baf5ed8a9ab59467b25583e5104
lzzzzzq <1441001905@qq.com>
2019-03-12 10:44:10 +0800

    revert: 回退 badge 组件的改动

--- src/views/docs/components/badge/demo.vue
+++ src/views/docs/components/badge/demo.vue
@@ -3,50 +3,75 @@
     <p-white-space></p-white-space>
     <p-badge>1234</p-badge>
     &nbsp;&nbsp;
+    <p-badge size="small">1234</p-badge>
+    &nbsp;&nbsp;
     <p-badge radiusCircle>1234</p-badge>
     &nbsp;&nbsp;
+    <p-badge size="small" radiusCircle>1234</p-badge>
 
     <p-white-space></p-white-space>
     <p-badge type="error">1234</p-badge>
     &nbsp;&nbsp;
+    <p-badge type="error" size="small">1234</p-badge>
+    &nbsp;&nbsp;
     <p-badge type="error" radiusCircle>1234</p-badge>
     &nbsp;&nbsp;
+    <p-badge type="error" size="small" radiusCircle>1234</p-badge>
 
     <p-white-space></p-white-space>
     <p-badge type="warning">1234</p-badge>
     &nbsp;&nbsp;
+    <p-badge type="warning" size="small">1234</p-badge>
+    &nbsp;&nbsp;
     <p-badge type="warning" radiusCircle>1234</p-badge>
     &nbsp;&nbsp;
+    <p-badge type="warning" size="small" radiusCircle>1234</p-badge>
 
     <p-white-space></p-white-space>
     <p-badge type="success">1234</p-badge>
     &nbsp;&nbsp;
+    <p-badge type="success" size="small">1234</p-badge>
+    &nbsp;&nbsp;
     <p-badge type="success" radiusCircle>1234</p-badge>
     &nbsp;&nbsp;
+    <p-badge type="success" size="small" radiusCircle>1234</p-badge>
 
     <p-white-space></p-white-space>
     <p-badge border>1234</p-badge>
     &nbsp;&nbsp;
+    <p-badge border size="small">1234</p-badge>
+    &nbsp;&nbsp;
     <p-badge border radiusCircle>1234</p-badge>
     &nbsp;&nbsp;
+    <p-badge border size="small" radiusCircle>1234</p-badge>
 
     <p-white-space></p-white-space>
     <p-badge type="error" border>1234</p-badge>
     &nbsp;&nbsp;
+    <p-badge type="error" border size="small">1234</p-badge>
+    &nbsp;&nbsp;
     <p-badge type="error" border radiusCircle>1234</p-badge>
     &nbsp;&nbsp;
+    <p-badge type="error" border size="small" radiusCircle>1234</p-badge>
 
     <p-white-space></p-white-space>
     <p-badge type="warning" border>1234</p-badge>
     &nbsp;&nbsp;
+    <p-badge type="warning" border size="small">1234</p-badge>
+    &nbsp;&nbsp;
     <p-badge type="warning" border radiusCircle>1234</p-badge>
     &nbsp;&nbsp;
+    <p-badge type="warning" border size="small" radiusCircle>1234</p-badge>
 
     <p-white-space></p-white-space>
     <p-badge type="success" border>1234</p-badge>
     &nbsp;&nbsp;
+    <p-badge type="success" border size="small">1234</p-badge>
+    &nbsp;&nbsp;
     <p-badge type="success" border radiusCircle>1234</p-badge>
     &nbsp;&nbsp;
+    <p-badge type="success" border size="small" radiusCircle>1234</p-badge>
+    <p-badge type="success" border size="small" radiusCircle></p-badge>
 
   </p-wing-blank>
 </template>
